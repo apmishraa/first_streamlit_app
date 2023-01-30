dd35d9b26abb2bbc6a5949eb64b133f89741cb6a
Anand Mishra <47292959+apmishraa@users.noreply.github.com>
2023-01-30 18:50:48 +0530

    Update streamlit_app.py
    added complete breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('Menu')
-streamlit.header('Breakfast')
-streamlit.text('Fruits and Juice')
-streamlit.text('That is All')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado and bread')
